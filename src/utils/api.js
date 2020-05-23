97f22d10651afb2a8837763b2411313d0195eaeb
Farid Safi <faridsafi@noreply.github.com>
2020-05-23 22:03:58 +0200

    pdf/make

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,7 @@ const { getConfig } = require('../config');
 module.exports.api = {
   post: function post(url, options) {
     const { domain, version, token, teamId } = getConfig();
+
     return got.post(`${domain}${url}`, {
       ...options,
       headers: {
